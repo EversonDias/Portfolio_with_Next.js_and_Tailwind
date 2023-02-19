7c0902fda1067d4adb64d95eaddb4322d5385e81
eversondias <eversondias@pop-os.localdomain>
2023-02-19 10:35:25 -0300

    createContext refacture function toggleTheme

--- src/partner/header/index.jsx
+++ src/partner/header/index.jsx
@@ -2,21 +2,12 @@ import React from 'react';
 import Container from './styles';
 import Logo from '../../components/Logo';
 import NavBar from '../../components/NavBar';
-import PropTypes from 'prop-types';
 
-export default function Header({toggleTheme, type}) {
+export default function Header() {
 	return (
 		<Container>
 			<Logo/>
-			<NavBar
-				type={type}
-				toggleTheme={toggleTheme}
-			/>
+			<NavBar/>
 		</Container>
 	);
 }
-
-Header.propTypes = {
-	toggleTheme: PropTypes.func.isRequired,
-	type: PropTypes.bool.isRequired,
-};
