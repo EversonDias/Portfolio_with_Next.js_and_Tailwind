71858793a75280442cfa4d90f94143d3b492ca2d
everson <eversond14s@gmail.com>
2023-05-22 22:46:48 -0300

    create carousel

--- my-project/src/components/Carousel/index.tsx
+++ my-project/src/components/Carousel/index.tsx
@@ -1,11 +1,10 @@
 export default function Carousel() {
   return (
     <div>
-      <div className="carousel w-[500px] h-[300px] flex">
+      <div className="carousel w-full h-[70vh] flex justify-center m-auto">
         {
           [...Array(5)].map((_item, index: number) => (
-            <div key={index} id={`item${index}`} className="carousel-item relative w-full">
-              <img src={`images/itemsCarousel/image${index}.webp`} className="w-full" alt={`${index}° imagem do carousel`} />
+            <div key={index} id={`item${index}`} className={`bg-cover bg-center carousel-item relative w-full imageCarousel${index}`}>
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href={`#item${index === 0 ? 4 : index - 1}`} className="btn btn-circle">❮</a>
                 <a href={`#item${index === 4 ? 0 : index + 1}`} className="btn btn-circle">❯</a>
@@ -14,13 +13,13 @@ export default function Carousel() {
           ))
         }
       </div>
-      {
-        [...Array(5)].map((_item, index: number) => (
-          <div className="flex py-2 gap-2 bg-slate-600 ">
-            <a href={`#item${index}`} className="btn btn-xs">{index}</a>
-          </div>
-        ))
-      }
+      <div className="flex justify-center gap-3 mt-1">
+        {
+          [...Array(5)].map((_item, index: number) => (
+            <a href={`#item${index}`} className="btn btn-circle btn-xs bg-light-header hover:bg-light-highlights border-0 hover:text-light-font-2">{index}</a>
+          ))
+        }
+      </div>
     </div>
   )
 }
